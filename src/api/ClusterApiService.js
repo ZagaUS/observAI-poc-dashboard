60fa84400691ac60ce7865cafb600572d3f36c66
Mariselvamchellaiya <mariselvamchellaiya@gamil.com>
2024-02-23 16:33:10 +0530

    Status Page style changes

--- src/api/ClusterApiService.js
+++ src/api/ClusterApiService.js
@@ -43,3 +43,96 @@ export const changeToUninstrument = async (namespace, deploymentName) => {
     throw error;
   }
 };
+
+export const viewClusterInfoApiCall = async () => {
+  try {
+    const response = await axios.get(`${openshiftLoginURL}/viewClusterInfo`);
+
+    console.log("viewClusterInfoApiCall response", response.data);
+
+    return response;
+  } catch (error) {
+    console.error("Error in viewClusterInfoApiCall:", error);
+    throw error;
+  }
+};
+
+export const viewClusterIPApiCall = async () => {
+  try {
+    const response = await axios.get(`${openshiftLoginURL}/viewClusterIP`);
+
+    console.log("viewClusterIPApiCall response", response.data);
+
+    return response;
+  } catch (error) {
+    console.error("Error in viewClusterIPApiCall:", error);
+    throw error;
+  }
+};
+
+export const viewClusterInventoryApiCall = async () => {
+  try {
+    const response = await axios.get(
+      `${openshiftLoginURL}/viewClusterInventory`
+    );
+
+    console.log("viewClusterInventoryApiCall response", response.data);
+
+    return response;
+  } catch (error) {
+    console.error("Error in viewClusterInventoryApiCall:", error);
+    throw error;
+  }
+};
+
+export const viewClusterNetworkApiCall = async () => {
+  try {
+    const response = await axios.get(`${openshiftLoginURL}/viewClusterNetwork`);
+
+    console.log("viewClusterNetworkApiCall response", response.data);
+
+    return response;
+  } catch (error) {
+    console.error("Error in viewClusterNetworkApiCall:", error);
+    throw error;
+  }
+};
+
+export const viewClusterNodesApiCall = async () => {
+  try {
+    const response = await axios.get(`${openshiftLoginURL}/viewClusterNodes`);
+
+    console.log("viewClusterNodesApiCall response", response.data);
+
+    return response;
+  } catch (error) {
+    console.error("Error in viewClusterNodesApiCall:", error);
+    throw error;
+  }
+};
+
+export const viewClusterStatusApiCall = async () => {
+  try {
+    const response = await axios.get(`${openshiftLoginURL}/viewClusterStatus`);
+
+    console.log("viewClusterStatusApiCall response", response.data);
+
+    return response;
+  } catch (error) {
+    console.error("Error in viewClusterStatusApiCall:", error);
+    throw error;
+  }
+};
+
+export const viewClusterNodeIPApiCall = async () => {
+  try {
+    const response = await axios.get(`${openshiftLoginURL}/viewNodeIP`);
+
+    console.log("viewNodeIPApiCall response", response.data);
+
+    return response;
+  } catch (error) {
+    console.error("Error in viewNodeIPApiCall:", error);
+    throw error;
+  }
+};
